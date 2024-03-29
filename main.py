e3878f15cfc6248e327a98ded99aa9a44e6ac060
zbx <2694735157@qq.com>
2024-03-29 16:08:21 +0800

    init

---
+++ main.py
@@ -0,0 +1,135 @@
+import os
+import re
+import sys
+import time
+
+import cv2
+import numpy
+import pyautogui
+import shutil
+import atexit
+
+GUN_MAT_MAP = {}
+BOX_MAT: cv2.Mat
+TURBO_MAT: cv2.Mat
+
+
+# 获取屏幕指定位置截图Mat
+def getScreenMat(loc: tuple[int, int, int, int]):
+    img = pyautogui.screenshot(region=loc).convert('RGBA')
+    for y in range(img.height):
+        for x in range(img.width):
+            r, g, b, a = img.getpixel((x, y))
+            r = 255 if r > 200 else 0
+            g = 255 if g > 200 else 0
+            b = 255 if b > 200 else 0
+            a = 0 if r == 0 and g == 0 and b == 0 else a
+            img.putpixel((x, y), (r, g, b, a))
+    return cv2.cvtColor(numpy.array(img), cv2.COLOR_RGB2BGR)
+
+
+# 加载匹配模板材质Mat
+def loadMat():
+    enable = open("./script/gun.lua", 'r', encoding='utf-8')
+    lines = enable.readlines()
+    enable.close()
+    enable_list = list()
+    for line in lines:
+        if not line.startswith('GunTable'): continue
+        gun = re.findall(r'\'.+\'', line)
+        if len(gun) == 1:
+            enable_list.append(str(gun[0]).replace("'", ""))
+
+    for fileName in os.listdir("./weapon/2560x1440"):
+        if not fileName.endswith(".png"): continue
+        key = fileName.replace(".png", "")
+        if enable_list.__contains__(key):
+            gun_mat = cv2.imread("./weapon/2560x1440/" + fileName)
+            GUN_MAT_MAP[key] = gun_mat
+
+    global BOX_MAT
+    BOX_MAT = cv2.imread("./weapon/2560x1440/box.png")
+
+    global TURBO_MAT
+    TURBO_MAT = cv2.imread("./weapon/2560x1440/turbo.png")
+
+
+def getGunName(shot: cv2.Mat):
+    for key in GUN_MAT_MAP.keys():
+        if matchTemplate(shot, GUN_MAT_MAP[key]):
+            return key
+    return "none"
+
+
+def matchTemplate(shot: cv2.Mat, template: cv2.Mat):
+    res = cv2.matchTemplate(shot, template, cv2.TM_CCOEFF_NORMED)
+    _, max_val, _, _ = cv2.minMaxLoc(res)
+    return max_val > 0.77
+
+
+# 检查涡轮
+def checkTurbo(name: str, shot: cv2.Mat):
+    if name == "havoc" and matchTemplate(shot, TURBO_MAT):
+        return "\nturbo = 499"
+    elif name == "devotionLMG" and matchTemplate(shot, TURBO_MAT):
+        return "\nturbo = 399"
+    return ""
+
+
+def changeGunModel(content: str):
+    file = open("C:/Users/Public/Downloads/gunModel.lua", 'w', encoding='utf-8')
+    file.write(content)
+    file.close()
+    print('-> ', content)
+
+
+def whenExit():
+    try:
+        os.remove("C:/Users/Public/Downloads/gun.lua")
+        os.remove("C:/Users/Public/Downloads/gunModel.lua")
+    except FileNotFoundError:
+        pass
+    print('脚本卸载成功')
+
+
+def initScript():
+    shutil.copy("./script/gun.lua", "C:/Users/Public/Downloads/gun.lua")
+    shutil.copy("./script/gunModel.lua", "C:/Users/Public/Downloads/gunModel.lua")
+    atexit.register(whenExit)
+    print('脚本加载成功')
+
+
+def main():
+    last_content = ""
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            sys.exit(0)
+        # start = time.time()
+
+        this_content = "gunModel = "
+        box_shot = getScreenMat((140, 963, 140, 50))
+
+        if matchTemplate(box_shot, BOX_MAT):
+            this_content = "'none'"
+        else:
+            gun_shot = getScreenMat((1388, 779, 230, 90))
+            gun_name = getGunName(gun_shot)
+            this_content += f"'{gun_name}'"
+            other = checkTurbo(gun_name, gun_shot)
+            this_content += other
+
+        if not last_content.__eq__(this_content):
+            changeGunModel(this_content)
+            last_content = this_content
+
+        # print("耗时: {:.2f}秒".format(time.time() - start))
+
+
+if __name__ == '__main__':
+    loadMat()
+    initScript()
+
+    main()
---
+++ script/script.lua
@@ -0,0 +1,83 @@
+dofile("C:\\Users\\Public\\Downloads\\gun.lua")
+
+-- 休眠函数
+function Sleep(t)
+    local sleep_start_time = GetRunningTime()
+    repeat
+    until (GetRunningTime() - sleep_start_time > t)
+end
+
+-- 开火
+local function openFire()
+    -- 如果没有位置信息跳过
+    if (type(offset_list) == "nil") then return end
+    -- 开火状态
+    local fire = true
+    -- 第几枪
+    local ij = 1
+
+    -- 涡轮延迟
+    if (type(turbo) == "nil") then
+        Sleep(turbo)
+        turbine = nil
+    end
+
+    while (fire and ij <= #offset_list) do
+        offset = offset_list[ij];
+
+        for var_i = 1, offset[4] do
+            Sleep(offset_time)
+            MoveMouseRelative(offset[2], offset[3])
+        end
+
+        OutputLogMessage("第 " .. ij .. " 枪\n")
+
+        ij = ij + 1
+
+
+        fire = IsMouseButtonPressed(1)
+    end
+end
+
+-- 脚本入口
+function OnEvent(event, arg)
+    if (event == "PROFILE_ACTIVATED") then
+        ClearLog()
+
+        -- 启动左键事件, 不启动无法触发左键按下事件
+        EnablePrimaryMouseButtonEvents(true)
+
+        OutputLogMessage("脚本启动")
+    end
+
+    -- 检测小键盘锁是否启用, 未启用不执行脚本
+    if (not (IsKeyLockOn("numlock"))) then
+        return
+    end
+
+    -- 鼠标左键按下事件
+    if (event == "MOUSE_BUTTON_PRESSED" and arg == 1) then
+        ClearLog()
+        dofile("C:\\Users\\Public\\Downloads\\gunModel.lua")
+        OutputLogMessage(gunModel .. '\t')
+        if (type(gunModel) == "nil") then return end
+
+        offset_list = nil
+        offset_time = nil
+
+        -- 鼠标右键是否按下
+        if (IsMouseButtonPressed(3)) then
+            -- 瞄准模式
+            OutputLogMessage("瞄准模式\n")
+            offset_list = GunTable[gunModel][1]
+            offset_time = GunTable[gunModel][2]
+        else
+            -- 腰射模式
+            OutputLogMessage("腰射模式\n")
+            offset_list = GunTable[gunModel][3]
+            offset_time = GunTable[gunModel][4]
+        end
+
+        openFire()
+    end
+end
